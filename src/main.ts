55704d4b347afad15dbb83428dfe76555b7fe963
Seungjik Lee <lsjbh45@gmail.com>
2023-07-14 16:39:35 +0900

    feat: Add global exception filter and error logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationException } from './exceptions/validation.exception';
+import { AllExceptionsFilter } from './filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.useGlobalPipes(
     new ValidationPipe({
