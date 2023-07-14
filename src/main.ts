81f42d7001b91f71843a3a7edefd00606d7b543c
Seungjik Lee <lsjbh45@gmail.com>
2023-07-14 17:02:15 +0900

    chore: Add application listening port as config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './lib/filters/exception.filter';
 import { ValidationException } from './lib/exceptions/validation.exception';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('APP_PORT');
+
+  await app.listen(port);
 }
 bootstrap();
