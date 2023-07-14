f9aff1d9d059e100e3ec1675199d2cdf75ac7ca9
Seungjik Lee <lsjbh45@gmail.com>
2023-07-14 16:38:40 +0900

    feat: Add global validation pipe with transform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationException } from './exceptions/validation.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        return new ValidationException(errors);
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
