8262a8d19070cee7081a8d7cd8ffad67623a00fc
Seungjik Lee <lsjbh45@gmail.com>
2023-07-14 14:37:10 +0900

    chore: Add enviornment file configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+    }),
+  ],
+})
 export class AppModule {}
