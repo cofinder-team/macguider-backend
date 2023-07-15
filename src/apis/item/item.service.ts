987008d664d058576900f496aea5f4a274fb9457
Seungjik Lee <lsjbh45@gmail.com>
2023-07-15 15:52:55 +0900

    feat: Add detailed sub entities of item module

--- src/apis/item/item.service.ts
+++ src/apis/item/item.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ItemDetailEntity, ItemIpad, ItemMacbook } from 'src/entities';
 import { ItemRepository } from 'src/repositories';
+import { EntityNotFoundError, Repository } from 'typeorm';
 
 @Injectable()
 export class ItemService {
-  constructor(private readonly itemrepository: ItemRepository) {}
+  constructor(
+    private readonly itemrepository: ItemRepository,
+    @InjectRepository(ItemMacbook)
+    private readonly itemMacbookRepository: Repository<ItemMacbook>,
+    @InjectRepository(ItemIpad)
+    private readonly itemIpadRepository: Repository<ItemIpad>,
+  ) {}
+
+  private findRepositoryByType(type: string): Repository<ItemDetailEntity> {
+    switch (type) {
+      case 'M':
+        return this.itemMacbookRepository;
+      case 'P':
+        return this.itemIpadRepository;
+      default:
+        throw new EntityNotFoundError(ItemDetailEntity, type);
+    }
+  }
 }
