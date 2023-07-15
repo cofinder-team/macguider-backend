170fe38b2db81f04433f4c3e6479943afaa844e6
Seungjik Lee <lsjbh45@gmail.com>
2023-07-15 16:37:05 +0900

    feat: Add time restriction for getting deals

--- src/apis/deal/deal.service.ts
+++ src/apis/deal/deal.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Deal } from 'src/entities';
 import { DealRepository } from 'src/repositories';
+import { FindOptionsWhere } from 'typeorm';
 
 @Injectable()
 export class DealService {
   constructor(private readonly dealrepository: DealRepository) {}
 
   async getDeals(): Promise<Deal[]> {
-    return this.dealrepository.find({ order: { date: 'DESC' } });
+    const utcHour: number = new Date().getUTCHours();
+    const timeOption: FindOptionsWhere<Deal> =
+      utcHour >= 2 && utcHour < 6 ? {} : { sold: true };
+
+    return this.dealrepository.find({
+      where: { ...timeOption },
+      order: { date: 'DESC' },
+    });
   }
 
   async getDealImage(id: number): Promise<Buffer> {
