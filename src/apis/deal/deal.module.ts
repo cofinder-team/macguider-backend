a4aeaa8641f5e20d374a42160c3d87a6c3380417
Seungjik Lee <lsjbh45@gmail.com>
2023-07-16 17:26:49 +0900

    feat: Add base structure of raw deal entity

--- src/apis/deal/deal.module.ts
+++ src/apis/deal/deal.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DealRepository } from 'src/repositories';
 import { DealController } from './deal.controller';
 import { DealService } from './deal.service';
+import { DealRaw } from 'src/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DealRepository])],
+  imports: [TypeOrmModule.forFeature([DealRepository, DealRaw])],
   controllers: [DealController],
   providers: [DealService, DealRepository],
 })
