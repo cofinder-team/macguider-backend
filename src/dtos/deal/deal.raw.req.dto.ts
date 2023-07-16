ed51f90298005790282ac0cb6552c7fd7bfa28ba
Seungjik Lee <lsjbh45@gmail.com>
2023-07-16 17:29:59 +0900

    feat: Add api for converting raw deal to real deal

---
+++ src/dtos/deal/deal.raw.req.dto.ts
@@ -0,0 +1,22 @@
+import { Type } from 'class-transformer';
+import { IsBoolean, IsIn, IsNumber, IsOptional } from 'class-validator';
+
+export class DealRawRequestDto {
+  @IsBoolean()
+  @Type(() => Boolean)
+  valid: boolean;
+
+  @IsOptional()
+  @IsIn(['M', 'P'])
+  type?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  itemId?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  unopened?: boolean;
+}
