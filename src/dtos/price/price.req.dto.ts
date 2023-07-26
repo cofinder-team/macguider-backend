58d797695791f9d8ff9b55d7011b96e0b9d0868a
Seungjik Lee <lsjbh45@gmail.com>
2023-07-26 19:09:00 +0900

    feat: Add generalized api for getting trade price

--- src/dtos/price/price.req.dto.ts
+++ src/dtos/price/price.req.dto.ts
@@ -1,9 +1,18 @@
 import { Transform } from 'class-transformer';
-import { IsBoolean, IsOptional } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class PriceRequestDto {
   @IsOptional()
   @IsBoolean()
   @Transform(({ value }) => value === 'true')
   unused?: boolean = false;
+
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => value === 'true')
+  singular?: boolean = false;
+
+  @IsOptional()
+  @IsString()
+  source?: string;
 }
