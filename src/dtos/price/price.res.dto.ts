ded2a6f48be08cfb9e2ec4f3a86f0ca27892376a
Seungjik Lee <lsjbh45@gmail.com>
2023-07-26 19:10:10 +0900

    feat: Add date to trade price response

--- src/dtos/price/price.res.dto.ts
+++ src/dtos/price/price.res.dto.ts
@@ -1,13 +1,14 @@
 import { PriceTrade } from 'src/entities';
 
 export class PriceResponseDto {
+  date: Date;
   average: number;
   price_20: number;
   price_80: number;
   cnt: number;
 
   static of(price: PriceTrade): PriceResponseDto {
-    const { average, price_20, price_80, cnt } = price;
-    return { average, price_20, price_80, cnt };
+    const { date, average, price_20, price_80, cnt } = price;
+    return { date, average, price_20, price_80, cnt };
   }
 }
