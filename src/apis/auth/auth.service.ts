339049a0696d8987b213e7d4be803580d0748869
Seungjik Lee <lsjbh45@gmail.com>
2023-07-29 14:58:22 +0900

    feat: Add hashing and verifying password

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  async verifyPassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
