ee36b0684e0577d9ee3d6e61909d324ee55a1025
Seungjik Lee <lsjbh45@gmail.com>
2023-07-29 15:01:23 +0900

    feat: Add base logics for registration

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,7 +1,13 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ schema: 'macguider', name: 'user' })
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 }
