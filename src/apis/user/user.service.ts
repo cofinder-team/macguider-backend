9f52c42a59f1a100bddd5a7a6b17a1003a33a944
Seungjik Lee <lsjbh45@gmail.com>
2023-07-31 14:48:23 +0900

    feat: Add checking refresh token from database

--- src/apis/user/user.service.ts
+++ src/apis/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { TokenPayloadDto } from 'src/dtos';
 import { User } from 'src/entities';
 import { UserRepository } from 'src/repositories';
 
@@ -11,8 +10,8 @@ export class UserService {
     return this.userRepository.findOne({ where: { email } });
   }
 
-  async getUserByIdEmail(id: number, email: string): Promise<User> {
-    return this.userRepository.findOne({ where: { id, email } });
+  async getUserById(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
   }
 
   async createUser(user: Partial<User>): Promise<User> {
@@ -22,11 +21,5 @@ export class UserService {
   async checkDuplicationEmail(email: string): Promise<boolean> {
     const user = await this.getUserByEmail(email);
     return !user;
-  }
-
-  async verifyTokenUser(token: TokenPayloadDto): Promise<boolean> {
-    const { id, email } = token;
-    const user = await this.getUserByIdEmail(id, email);
-    return !!user;
   }
 }
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -10,4 +10,7 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @Column({ nullable: true })
+  refreshToken: string;
 }
