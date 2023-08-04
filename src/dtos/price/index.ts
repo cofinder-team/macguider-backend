32c69c6d6ffbe15cd21910b3eb8125380ee18002
Seungjik Lee <lsjbh45@gmail.com>
2023-08-04 12:42:14 +0900

    feat: Add dtos for remaining price entities

--- src/dtos/price/index.ts
+++ src/dtos/price/index.ts
@@ -1,4 +1,15 @@
+import { PriceCoupangRequestDto } from './price.coupang.req.dto';
+import { PriceCoupangResponseDto } from './price.coupang.res.dto';
+import { PriceRegularRequestDto } from './price.regular.req.dto';
+import { PriceRegularResponseDto } from './price.regular.res.dto';
 import { PriceTradeRequestDto } from './price.trade.req.dto';
 import { PriceTradeResponseDto } from './price.trade.res.dto';
 
-export { PriceTradeRequestDto, PriceTradeResponseDto };
+export {
+  PriceRegularRequestDto,
+  PriceRegularResponseDto,
+  PriceCoupangRequestDto,
+  PriceCoupangResponseDto,
+  PriceTradeRequestDto,
+  PriceTradeResponseDto,
+};
