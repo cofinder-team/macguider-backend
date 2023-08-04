6fb4f54fe205029784c638bf6bc16a927c13e610
Seungjik Lee <lsjbh45@gmail.com>
2023-08-04 12:42:26 +0900

    feat: Add interfaces for remaining price entities

--- src/apis/price/price.controller.ts
+++ src/apis/price/price.controller.ts
@@ -1,10 +1,56 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { PriceService } from './price.service';
-import { ItemDto, PriceTradeRequestDto, PriceTradeResponseDto } from 'src/dtos';
+import {
+  ItemDto,
+  PriceCoupangRequestDto,
+  PriceCoupangResponseDto,
+  PriceRegularRequestDto,
+  PriceRegularResponseDto,
+  PriceTradeRequestDto,
+  PriceTradeResponseDto,
+} from 'src/dtos';
 
 @Controller('price')
 export class PriceController {
   constructor(private readonly itemService: PriceService) {}
+
+  @Get('/regular/:type/:id')
+  async getRegularPrices(
+    @Param() item: ItemDto,
+    @Query() query: PriceRegularRequestDto,
+  ): Promise<PriceRegularResponseDto[] | PriceRegularResponseDto> {
+    const { type, id } = item;
+    const { singular } = query;
+
+    const options = { type, id };
+
+    if (singular) {
+      const price = await this.itemService.getRecentRegularPrice(options);
+      return PriceRegularResponseDto.of(price);
+    } else {
+      const prices = await this.itemService.getRegularPrices(options);
+      return prices.map(PriceRegularResponseDto.of);
+    }
+  }
+
+  @Get('/coupang/:type/:id')
+  async getCoupangPrices(
+    @Param() item: ItemDto,
+    @Query() query: PriceCoupangRequestDto,
+  ): Promise<PriceCoupangResponseDto[] | PriceCoupangResponseDto> {
+    const { type, id } = item;
+    const { singular } = query;
+
+    const options = { type, id };
+
+    if (singular) {
+      const price = await this.itemService.getRecentCoupangPrice(options);
+      return PriceCoupangResponseDto.of(price);
+    } else {
+      const prices = await this.itemService.getCoupangPrices(options);
+      return prices.map(PriceCoupangResponseDto.of);
+    }
+  }
 
   @Get('/trade/:type/:id')
   async getTradePrices(
@@ -12,9 +58,9 @@ export class PriceController {
     @Query() query: PriceTradeRequestDto,
   ): Promise<PriceTradeResponseDto[] | PriceTradeResponseDto> {
     const { type, id } = item;
-    const { unused, singular, source } = query;
+    const { singular, unused, source } = query;
 
-    const options = { type, id, unused, source: source ?? IsNull() };
+    const options = { type, id, unused, source };
 
     if (singular) {
       const price = await this.itemService.getRecentTradePrice(options);
@@ -33,7 +79,7 @@ export class PriceController {
     const { type, id } = params;
     const { unused } = query;
 
-    const options = { type, id, unused, source: IsNull() };
+    const options = { type, id, unused };
 
     const price = await this.itemService.getRecentTradePrice(options);
     return PriceTradeResponseDto.of(price);
