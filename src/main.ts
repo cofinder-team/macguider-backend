bb47e3f7309222e704fe163c0d71272ba7ba7875
Seungjik Lee <lsjbh45@gmail.com>
2023-08-04 16:28:21 +0900

    feat: Apply swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './lib/filters/exception.filter';
 import { ValidationException } from './lib/exceptions/validation.exception';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
+  const env = configService.get<string>('NODE_ENV');
+  const port = configService.get<number>('APP_PORT');
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
@@ -21,16 +25,18 @@ async function bootstrap() {
     }),
   );
 
-  const corsOrigin =
-    configService.get<string>('NODE_ENV') === 'prod'
-      ? ['https://www.macguider.io']
-      : '*';
+  const corsOrigin = env === 'dev' ? '*' : ['https://www.macguider.io'];
   app.enableCors({
     origin: corsOrigin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
-  const port = configService.get<number>('APP_PORT');
+  if (env === 'dev') {
+    const config = new DocumentBuilder().setTitle('Macguider API').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(port);
 }
 bootstrap();
