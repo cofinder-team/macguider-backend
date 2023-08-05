77a6a36f63529095e8882603c411bda11bd22218
Seungjik Lee <lsjbh45@gmail.com>
2023-08-05 18:21:17 +0900

    refactor: Distinguish endpoint of single price

--- src/dtos/price/price.req.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Transform } from 'class-transformer';
-import { IsBoolean, IsOptional } from 'class-validator';
-
-export abstract class PriceRequestDto {
-  @IsOptional()
-  @IsBoolean()
-  @Transform(({ value }) => value === 'true')
-  singular?: boolean = false;
-}
