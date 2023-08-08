7335025f2d279935e0503814953c76bb739b1270
Seungjik Lee <lsjbh45@gmail.com>
2023-08-08 10:10:44 +0900

    feat: Add interface for creating new alert target

--- src/apis/alert/alert.controller.ts
+++ src/apis/alert/alert.controller.ts
@@ -1,9 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AlertService } from './alert.service';
-import { ApiTags } from '@nestjs/swagger';
+import { AuthUser } from 'src/lib/decorators/auth.user.decorator';
+import { TokenPayloadDto } from 'src/dtos';
+import { JwtAuthGuard } from '../auth/jwt/jwt.auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AlertCreateRequestDto, AlertResponseDto } from 'src/dtos/alert';
 
 @Controller('alert')
 @ApiTags('alert')
 export class AlertController {
   constructor(private readonly alertService: AlertService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '사용자의 알림 대상 옵션 추가' })
+  @ApiBearerAuth()
+  async createAlert(
+    @AuthUser() user: TokenPayloadDto,
+    @Body() body: AlertCreateRequestDto,
+  ): Promise<AlertResponseDto> {
+    const { id: userId } = user;
+    const { type, id: itemId, unused } = body;
+
+    const alert = { type, itemId, unused, userId };
+    const result = await this.alertService.createAlert(alert);
+    return AlertResponseDto.of(result);
+  }
 }
---
+++ src/dtos/alert/index.ts
@@ -0,0 +1,4 @@
+import { AlertCreateRequestDto } from './alert.create.req.dto';
+import { AlertResponseDto } from './alert.res.dto';
+
+export { AlertCreateRequestDto, AlertResponseDto };
