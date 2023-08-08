caf30c6b05af188918212a9435bd60c2069ce6cc
Seungjik Lee <lsjbh45@gmail.com>
2023-08-08 12:09:39 +0900

    fix: Remove uuid from alert response dto

--- src/dtos/alert/alert.res.dto.ts
+++ src/dtos/alert/alert.res.dto.ts
@@ -6,17 +6,15 @@ export class AlertResponseDto {
   item: ItemResponseDto;
   unused: boolean;
   userId: number;
-  uuid?: string;
 
   static of(alert: AlertTarget): AlertResponseDto {
-    const { id, type, itemId, unused, userId, uuid, item } = alert;
+    const { id, type, itemId, unused, userId, item } = alert;
 
     return {
       id,
       item: { ...(item ? ItemResponseDto.of(item) : { type, id: itemId }) },
       unused,
       userId,
-      uuid,
     };
   }
 }
