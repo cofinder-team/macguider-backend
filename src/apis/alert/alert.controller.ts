95374f60316c23abfeba0b7e60b6f426e3352a43
Seungjik Lee <lsjbh45@gmail.com>
2023-08-08 11:19:16 +0900

    feat: Add item existence validation when add alert

--- src/apis/alert/alert.controller.ts
+++ src/apis/alert/alert.controller.ts
@@ -6,11 +6,15 @@ import { JwtAuthGuard } from '../auth/jwt/jwt.auth.guard';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AlertCreateRequestDto, AlertResponseDto } from 'src/dtos/alert';
 import { randomUuid } from 'src/lib/utils/uuid.util';
+import { ItemService } from '../item/item.service';
 
 @Controller('alert')
 @ApiTags('alert')
 export class AlertController {
-  constructor(private readonly alertService: AlertService) {}
+  constructor(
+    private readonly alertService: AlertService,
+    private readonly itemService: ItemService,
+  ) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
@@ -35,6 +39,8 @@ export class AlertController {
   ): Promise<AlertResponseDto> {
     const { id: userId } = user;
     const { type, id: itemId, unused } = body;
+
+    await this.itemService.existsItem(type, itemId);
 
     const alert = { type, itemId, unused, userId, uuid: randomUuid() };
     const result = await this.alertService.createAlert(alert);
