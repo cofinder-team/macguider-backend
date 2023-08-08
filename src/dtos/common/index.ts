9d046cc651a5f167243208a7aa8248e6fe38153a
Seungjik Lee <lsjbh45@gmail.com>
2023-08-08 12:08:06 +0900

    refactor: Clarify internal authenticated user type

--- src/dtos/common/index.ts
+++ src/dtos/common/index.ts
@@ -1,5 +1,6 @@
+import { AuthUserDto } from './auth.user.dto';
 import { ItemDto } from './item.dto';
 import { PaginationDto } from './pagination.dto';
 import { TokenPayloadDto } from './token.payload.dto';
 
-export { ItemDto, PaginationDto, TokenPayloadDto };
+export { ItemDto, PaginationDto, TokenPayloadDto, AuthUserDto };
