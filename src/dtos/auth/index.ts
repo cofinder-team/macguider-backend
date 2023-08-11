d0b3271c80b095e19e8ce99484041c2e2c232ff3
Seungjik Lee <lsjbh45@gmail.com>
2023-08-11 12:27:15 +0900

    feat: Add interface for renewing certificate email

--- src/dtos/auth/index.ts
+++ src/dtos/auth/index.ts
@@ -2,11 +2,13 @@ import { AuthCertificateRequestDto } from './auth.certificate.req.dto';
 import { AuthLoginRequestDto } from './auth.login.req.dto';
 import { AuthRefreshRequestDto } from './auth.refresh.req.dto';
 import { AuthRegisterRequestDto } from './auth.register.req.dto';
+import { AuthResendRequestDto } from './auth.resend.req.dto';
 import { AuthTokenResponseDto } from './auth.token.res.dto';
 
 export {
   AuthLoginRequestDto,
   AuthRegisterRequestDto,
+  AuthResendRequestDto,
   AuthRefreshRequestDto,
   AuthTokenResponseDto,
   AuthCertificateRequestDto,
