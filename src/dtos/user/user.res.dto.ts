e0fcfa5f581a368fc6afb49328c8fd75baaf2d3c
Seungjik Lee <lsjbh45@gmail.com>
2023-08-11 14:42:00 +0900

    feat: Add interface for getting auth user info

--- src/dtos/user/user.res.dto.ts
+++ src/dtos/user/user.res.dto.ts
@@ -3,9 +3,10 @@ import { User } from 'src/entities';
 export class UserResponseDto {
   id: number;
   email: string;
+  role: string;
 
   static of(user: User): UserResponseDto {
-    const { id, email } = user;
-    return { id, email };
+    const { id, email, role } = user;
+    return { id, email, role };
   }
 }
