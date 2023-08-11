78867a2d004800099439ca06b0f133017d723c44
Seungjik Lee <lsjbh45@gmail.com>
2023-08-11 12:50:04 +0900

    refactor: Remove unnecessary custom repositories

--- src/apis/user/user.service.ts
+++ src/apis/user/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities';
-import { UserRepository } from 'src/repositories';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   async getUserByEmail(email: string): Promise<User> {
     return this.userRepository.findOne({ where: { email } });
