6f490053f8178de4cfa8282fa21b66d13fbf7168
Seungjik Lee <lsjbh45@gmail.com>
2023-08-30 19:14:11 +0900

    feat: Add enums for existing entities

---
+++ src/lib/enums/chip.enum.ts
@@ -0,0 +1,13 @@
+export enum Chip {
+  M1 = 'M1',
+  M1PRO = 'M1PRO',
+  M1MAX = 'M1MAX',
+  M2 = 'M2',
+  M2PRO = 'M2PRO',
+  M2MAX = 'M2MAX',
+  A12 = 'A12',
+  A12Z = 'A12Z',
+  A13 = 'A13',
+  A14 = 'A14',
+  A15 = 'A15',
+}
---
+++ src/lib/enums/ssd.enum.ts
@@ -0,0 +1,7 @@
+export enum Ssd {
+  _256GB = '256GB',
+  _512GB = '512GB',
+  _1TB = '1TB',
+  _2TB = '2TB',
+  _4TB = '4TB',
+}
