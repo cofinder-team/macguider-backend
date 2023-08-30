b5edcabfb9530e8e0120ad1738189d05a3f3c473
Seungjik Lee <lsjbh45@gmail.com>
2023-08-30 19:14:11 +0900

    feat: Add enums for existing entities

---
+++ src/lib/enums/index.ts
@@ -0,0 +1,20 @@
+import { Chip } from './chip.enum';
+import { IphoneSuffix } from './iphone.suffix.enum';
+import { ItemCondition } from './item.condition.enum';
+import { ItemType, getItemType } from './item.type.enum';
+import { Source } from './source.enum';
+import { Ssd } from './ssd.enum';
+import { Storage } from './storage.enum';
+import { Role } from './user.role.enum';
+
+export {
+  ItemType,
+  getItemType,
+  ItemCondition,
+  Source,
+  Role,
+  Chip,
+  Ssd,
+  Storage,
+  IphoneSuffix,
+};
---
+++ src/lib/enums/storage.enum.ts
@@ -0,0 +1,9 @@
+export enum Storage {
+  _32GB = '32GB',
+  _64GB = '64GB',
+  _128GB = '128GB',
+  _256GB = '256GB',
+  _512GB = '512GB',
+  _1TB = '1TB',
+  _2TB = '2TB',
+}
