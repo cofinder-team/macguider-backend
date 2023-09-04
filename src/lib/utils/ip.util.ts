ba8e1709d4d1d6a8e06057f7574c21df55c145b6
Seungjik Lee <lsjbh45@gmail.com>
2023-09-04 16:30:23 +0900

    fix: Resolve problem caused by x-forwarded-for

--- src/lib/utils/ip.util.ts
+++ src/lib/utils/ip.util.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 
 const getClientIp = (request: Request): string =>
-  (request.headers['x-forwarded-for'] as string) ||
+  (request.headers['x-forwarded-for'] as string)?.split(',')[0] ||
   request.socket.remoteAddress;
 
 export { getClientIp };
