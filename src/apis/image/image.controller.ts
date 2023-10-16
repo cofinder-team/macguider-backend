6bf62c16d3379126e6837ad2832549cfc4c9586c
Seungjik Lee <lsjbh45@gmail.com>
2023-10-16 16:27:55 +0900

    feat: Add interface for uploading new image to aws

--- src/apis/image/image.controller.ts
+++ src/apis/image/image.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+} from '@nestjs/common';
 import { S3Service } from './s3/s3.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { InterceptFile } from 'src/lib/decorators/file.decorator';
+import { ImageS3RequestDto } from 'src/dtos/image/image.s3.req.dto';
+import { randomUuid } from 'src/lib/utils/uuid.util';
+import { ImageType, parseImageType } from 'src/lib/enums/image.type.enum';
 import { ImageService } from './image.service';
 
 @Controller('image')
@@ -10,4 +19,26 @@ export class ImageController {
     private readonly s3Service: S3Service,
     private readonly imageService: ImageService,
   ) {}
+
+  @Post()
+  @ApiOperation({ summary: '이미지 업로드' })
+  @InterceptFile('file')
+  async uploadImage(@UploadedFile() file: Express.Multer.File): Promise<void> {
+    const { mimetype, buffer } = file;
+
+    const key: string = randomUuid();
+    const type: ImageType = parseImageType(mimetype);
+    if (!type) {
+      throw new BadRequestException('지원하지 않는 이미지 타입입니다.');
+    }
+
+    const image: ImageS3RequestDto = {
+      key,
+      type,
+      buffer,
+    };
+
+    await this.s3Service.uploadS3Image(image);
+    await this.imageService.createImage({ key, type });
+  }
 }
