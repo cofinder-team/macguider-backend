9fe186136ddf55e48738477ab0f3e762e1f225cf
Seungjik Lee <lsjbh45@gmail.com>
2023-10-19 17:04:57 +0900

    refactor: Modify image url to be composited

--- src/dtos/image/image.res.dto.ts
+++ src/dtos/image/image.res.dto.ts
@@ -1,10 +1,12 @@
 import { Image } from 'src/entities';
+import { getExtensionByImageType } from 'src/lib/enums/image.type.enum';
 
 export class ImageResponseDto {
   url: string;
 
   static of(image: Image): ImageResponseDto {
-    const { url } = image;
+    const { key, type } = image;
+    const url = `${key}.${getExtensionByImageType(type)}`;
     return { url };
   }
 }
